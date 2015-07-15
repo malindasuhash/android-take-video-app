656de9cf15dd62d1b38a5a7ca818713e50bde134
malindasuhash <malinda.suhash@gmail.com>
2015-07-15 06:04:09 +0100

    Adding a link to source code repo

--- app/src/main/java/mooc/spring/malinda/thevideoapp/activities/MainActivity.java
+++ app/src/main/java/mooc/spring/malinda/thevideoapp/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
@@ -58,6 +59,9 @@ public class MainActivity extends ConfigurationHandledActivity<VideoOps> {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent showSource = new Intent(Intent.ACTION_VIEW);
+            showSource.setData(Uri.parse("https://github.com/malindasuhash/android-take-video-app"));
+            startActivity(showSource); // No need to check
             return true;
         }
 
