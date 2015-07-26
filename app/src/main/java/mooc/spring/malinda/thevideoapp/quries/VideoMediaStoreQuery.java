e94767cb0a93a64794b3bf4c6cd996a127d8cfc2
malindasuhash <malinda.suhash@gmail.com>
2015-07-26 20:18:31 +0100

    moved some code over

--- app/src/main/java/mooc/spring/malinda/thevideoapp/quries/VideoMediaStoreQuery.java
+++ app/src/main/java/mooc/spring/malinda/thevideoapp/quries/VideoMediaStoreQuery.java
@@ -1,5 +1,11 @@
 package mooc.spring.malinda.thevideoapp.quries;
 
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+
 import mooc.spring.malinda.thevideoapp.operations.models.MediaStoreVideo;
 
 /**
@@ -7,8 +13,36 @@ import mooc.spring.malinda.thevideoapp.operations.models.MediaStoreVideo;
  */
 public class VideoMediaStoreQuery {
 
+    private Context context;
+
+    public VideoMediaStoreQuery(Context context)
+    {
+
+        this.context = context;
+    }
+
     public MediaStoreVideo getVideoById(long id)
     {
-        return null;
+        // Build the Uri to the Video having given video Id.
+        Uri videoUri = ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id);
+
+        // Cursor that is returned as a result of database query which
+        // points to one row.
+        try (Cursor cursor = context.getContentResolver().query(videoUri,
+                             null,
+                             null,
+                             null,
+                             null)) {
+            // Check if there is any row returned by the query.
+            if (cursor.moveToFirst())
+                // Get the Video metadata from Android Video Content
+                // Provider
+                return null;
+                //return getVideo(cursor);
+            else
+                // Return null if there id no row returned by the
+                // Query.
+                return null;
+        }
     }
 }
